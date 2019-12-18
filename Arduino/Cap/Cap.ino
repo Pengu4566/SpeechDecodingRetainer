e30ab37f8e231485ebe2b8d6bfe91007f33573ee
Pengu4566 <50245568+Pengu4566@users.noreply.github.com>
2019-12-18 13:47:20 -0600

    Add files via upload

---
+++ Arduino/Cap/Cap.ino
@@ -0,0 +1,156 @@
+#include <CapacitiveSensor.h>
+#include <U8x8lib.h>
+#include <HID.h>
+
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+const uint8_t colLow = 4;
+const uint8_t colHigh = 13;
+const uint8_t rowCups = 0;
+const uint8_t rowState = 2;
+const uint8_t rowTemp = 4;
+const uint8_t rowTime = 6;
+
+// 10M resistor between pins 5 & 10, pin 10 is sensor pin
+
+CapacitiveSensor btn11 = CapacitiveSensor(5,A5);
+
+int CAP_TRIGGER_VALUE = 3000;
+char KEY_PRESSED = ' ';
+char KEY_PRESSED_2 = ' ';
+long FIRST = 0;
+long SECOND = 0;
+double TOTAL = 0;  
+int FLAG = 0; 
+
+void lcdBold(bool aVal) 
+{
+  if (aVal) 
+  {
+    u8x8.setFont(u8x8_font_victoriabold8_r); //BOLD
+  } 
+  else 
+  {
+    u8x8.setFont(u8x8_font_victoriamedium8_r); //NORMAL
+  }
+}
+
+void lcdRun() 
+{
+  lcdBold(false);
+}
+
+void setup()                    
+{
+  Serial.begin(9600);
+  FLAG = 0;
+  u8x8.begin();
+  lcdBold(true);
+  u8x8.clear();
+  u8x8.setCursor(0,rowCups);
+  u8x8.print(F("Calc & Keyboard"));
+  u8x8.setCursor(0,rowState);
+  u8x8.print(__DATE__);
+  u8x8.setCursor(0,rowTemp);
+  u8x8.print(__TIME__);
+  lcdRun();
+}
+
+void loop()                    
+{
+    long start = millis();
+    long btnVal11 =  btn11.capacitiveSensor(30);
+
+    
+    Serial.println(btnVal11);
+
+    
+
+    KEY_PRESSED = GetInput(btnVal11, CAP_TRIGGER_VALUE);
+
+    //calculator
+    if(FLAG == 0){
+      switch(KEY_PRESSED) 
+      {
+        case '0' ... '9': 
+          FIRST = FIRST * 10 + (KEY_PRESSED - '0');
+          
+          u8x8.clear(); 
+          u8x8.setCursor(0,rowCups); 
+          u8x8.println(FIRST);
+          //Serial.println(FIRST);
+          break;
+          
+        case '+':
+          u8x8.setCursor(0,2); 
+          u8x8.println(F("+"));
+                  
+          FIRST = (TOTAL != 0 ? TOTAL : FIRST);
+          SECOND = SecondNumber();
+          TOTAL = FIRST + SECOND;
+  
+  
+          FIRST = 0, SECOND = 0;
+  
+          //u8x8.clear(); 
+          u8x8.setCursor(0,6); 
+          u8x8.println(TOTAL);
+  
+          TOTAL = 0;
+  
+          break;
+       }
+       
+       if(KEY_PRESSED == '*') 
+       {
+          FLAG = 1;
+       }
+    }
+    //t9 keyboard
+    else if (FLAG == 1)
+    {
+       Serial.println("T9 KEYBOARD");
+       FLAG = 0;
+    }
+
+    KEY_PRESSED = ' ';
+    TOTAL = 0;
+    delay(100);
+}
+
+char GetInput(long btnVal11, int CAP_TRIGGER_VALUE)
+{
+  
+  if (btnVal11 > CAP_TRIGGER_VALUE){KEY_PRESSED = '*';}
+
+  //Serial.println(KEY_PRESSED);
+  return KEY_PRESSED;
+}
+
+long SecondNumber()
+{
+  while(1)
+  {
+    delay(100);
+    long btnVal11 =  btn11.capacitiveSensor(30);
+    
+    if (btnVal11 > CAP_TRIGGER_VALUE){KEY_PRESSED_2 = '*';}
+
+    if(KEY_PRESSED_2 >= '0' && KEY_PRESSED_2 <= '9')
+    {
+      SECOND = SECOND * 10 + (KEY_PRESSED_2 - '0');
+      //Serial.println(SECOND);
+      u8x8.setCursor(0,4); 
+      u8x8.println(SECOND);
+    }
+    
+    if(KEY_PRESSED_2 == '=') 
+    {
+      KEY_PRESSED_2 = ' ';
+      break;
+    }
+      
+    KEY_PRESSED_2 = ' ';
+  }
+  
+  return SECOND; 
+}
---
+++ Arduino/MouthCarControlSend/MouthCarControlSend.ino
@@ -0,0 +1,82 @@
+#include <CapacitiveSensor.h>
+#include <SoftwareSerial.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10 megohm between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50 kilohm - 50 megohm. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ * Best results are obtained if sensor foil and wire is covered with an insulator such as paper or plastic sheet
+ */
+ 
+SoftwareSerial BTSerial = SoftwareSerial(15,14); // RX | TX
+
+CapacitiveSensor   cs_3 = CapacitiveSensor(4,5);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+CapacitiveSensor   cs_4 = CapacitiveSensor(4,6);        // 10 megohm resistor between pins 4 & 6, pin 6 is sensor pin, add wire, foil
+CapacitiveSensor   cs_5 = CapacitiveSensor(4,7);        // 10 megohm resistor between pins 4 & 8, pin 8 is sensor pin, add wire, foil
+CapacitiveSensor   cs_6 = CapacitiveSensor(4,8); 
+
+int CAP_THRESHOLD = 300;
+
+char lastCommand = ' ';
+
+void setup()                    
+{
+   BTSerial.begin(9600); // HC-05 default speed in AT command more
+   BTSerial.println("AT\r\n");
+   
+   //cs_3.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+
+   delay(300);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_3.capacitiveSensor(30);
+    long total2 =  cs_4.capacitiveSensor(30);
+    long total3 =  cs_5.capacitiveSensor(30);
+    long total4 =  cs_6.capacitiveSensor(30);
+
+    if(total1 > CAP_THRESHOLD) {
+      BTSerial.write('f');
+      lastCommand = 'f';
+      Serial.println("forward");
+    }
+    else if(total2 > CAP_THRESHOLD) {
+      BTSerial.write('b');
+      lastCommand = 'b';
+      Serial.println("back");
+    }
+    else if(total3 > CAP_THRESHOLD) {
+      BTSerial.write('r');
+      lastCommand = 'r';
+      Serial.println("right");
+    }
+    else if(total4 > CAP_THRESHOLD) {
+      BTSerial.write('l');
+      lastCommand = 'l';
+      Serial.println("left");
+    }
+    else {
+      BTSerial.write('s');
+    }
+    
+
+    //Serial.print(millis() - start);        // check on performance in milliseconds
+
+    Serial.print(total1);                  // print sensor output 1
+    
+    Serial.print("\t");
+    Serial.print(total2);                  // print sensor output 2
+
+    Serial.print("\t");
+    Serial.print(total3);                // print sensor output 3
+    
+    Serial.print("\t");
+    Serial.println(total4);       
+
+    delay(100);                             // arbitrary delay to limit data to serial port 
+}
